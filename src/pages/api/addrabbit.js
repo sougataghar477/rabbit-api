d64a09e3f564321ade71f35f37abfca0b75bba8c
sougataghar477 <sougataghar47@gmail.com>
2023-02-25 09:24:39 +0530

    lower case

--- src/pages/api/addrabbit.js
+++ src/pages/api/addrabbit.js
@@ -4,6 +4,7 @@ dotenv.config();
 const { PORT } = process.env;
 export default async function handler(req, res) {
     let{breed,url,urlId} =req.body;
+    breed=breed.toLowerCase();
     console.log({breed,urlId});
     await db.collection("rabbits").insertOne({breed,url,urlId});
     res.status(200).json(`http://localhost:${PORT}/api/id/${urlId}`)
--- src/pages/api/breeds/[name].js
+++ src/pages/api/breeds/[name].js
@@ -1,6 +1,7 @@
 import db from "../../../utils/mongo";
 export default async function handler(req,res){
     let {name} =req.query;
+    name=name.toLowerCase();
     let collection=await db.collection("rabbits").find({breed:name}).toArray();
     res.status(200).json(collection)
 }
