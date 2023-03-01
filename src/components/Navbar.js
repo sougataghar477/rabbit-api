21624261986f06049cbe3fb33bc0c3f3733bdfa0
sougataghar477 <sougataghar47@gmail.com>
2023-03-01 21:02:18 +0530

    added title to the theme icon

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,26 +1,33 @@
-import { useEffect } from 'react';
-import { useColorMode,Flex, Spacer, Box,Text } from '@chakra-ui/react';
- import Link from 'next/link';
-function Navbar(){
-    useEffect(()=>{toggleColorMode()},[])
+import { useEffect } from "react";
+import { useColorMode, Flex, Spacer, Box, Text } from "@chakra-ui/react";
+import Link from "next/link";
+function Navbar() {
+    useEffect(() => {
+        toggleColorMode();
+    }, []);
     const { colorMode, toggleColorMode } = useColorMode();
-    return  <Box bg={colorMode==='light'?'blue.900':'blackAlpha.900'}>
+    return (
+        <Box bg={colorMode === "light" ? "blue.900" : "blackAlpha.900"}>
+            <Flex p={5} color={"white"} maxW={"960px"} mx={"auto"}>
+                <Link href="/">Rabbit API</Link>
+                <Spacer />
 
-    <Flex p={5} color={"white"} maxW={"960px"} mx={'auto'}>
-        <Link href='/'>Rabbit API</Link>
-        <Spacer/>
-        
-        <Flex columnGap={5}>
-        <Link href='/submit-image'>Submit images</Link>
-        <Link href='/docs'>Docs</Link>
-        {/* <button  onClick={toggleColorMode}>{colorMode!=='dark'?<SunIcon/>:<MoonIcon/>}</button> */}
-        <button  className={colorMode!=='light'&&'color-mode'} onClick={toggleColorMode}>
-            <Text fontSize={20}>{colorMode==='light'?'🌞':'🌛'}</Text>
-            </button>
-        </Flex>
-
-    </Flex>
-    </Box>
-
+                <Flex columnGap={5}>
+                    <Link href="/submit-image">Submit images</Link>
+                    <Link href="/docs">Docs</Link>
+                    <button
+                        style={{
+                            fontSize: "20px",
+                            transform: colorMode==='light'?"rotate(0deg)":"rotate(30deg)"
+                        }}
+                        title="Click to toggle the mode."
+                        onClick={toggleColorMode}
+                    >
+                        {colorMode === "light" ? "🌞" : "🌛"}
+                    </button>
+                </Flex>
+            </Flex>
+        </Box>
+    );
 }
 export default Navbar;
