e088a09e6325bca0519dd30c48c4b7e4bb214a2f
sougataghar477 <sougataghar47@gmail.com>
2023-03-01 19:06:16 +0530

    added footer links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,8 +2,8 @@ import { Flex,Link } from "@chakra-ui/react";
 import {} from '@chakra-ui/icons';
 function Footer(){
     return <Flex display={['none','none','flex']} pos={'fixed'} right={6} bottom={1} columnGap={5} p={5} pr={10} justifyContent={['center','center','flex-end']}>
-        <Link  fontSize={18} fontStyle={'italic'}  ><u>Donate</u></Link>
-        <Link fontSize={18} fontStyle={'italic'}><u> Made by Sougata Ghar </u></Link>
+        <Link  fontSize={18} fontStyle={'italic'}  href='paypal.me/sougataghar47' isExternal><u>Donate</u></Link>
+        <Link fontSize={18} fontStyle={'italic'} href='https://github.com/sougataghar477/rabbit-api' isExternal><u> Made by Sougata Ghar </u></Link>
     </Flex>
 }
 export default Footer;
