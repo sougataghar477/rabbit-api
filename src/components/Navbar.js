494e9e1d91787f42d287b2be9a45750721389a84
sougata <sougataghar47@gmail.com>
2023-05-28 23:38:57 +0530

    added logo header

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,12 @@
 import { useEffect } from "react";
-import { useColorMode, Flex, Spacer, Box, Text,Image } from "@chakra-ui/react";
+import { useColorMode, Flex, Spacer, Box, Text, Image } from "@chakra-ui/react";
 import Link from "next/link";
 function Navbar() {
     const { colorMode, toggleColorMode } = useColorMode();
     return (
         <Box bg={colorMode === "light" ? "blue.900" : "blackAlpha.900"}>
             <Flex p={5} color={"white"} maxW={"960px"} mx={"auto"}>
-                <Link href="/"><Image w={12} src='/rrrrabbit.png' /></Link>
+                <Link href="/"><Flex alignItems={'center'}><Image w={12} src='/rrrrabbit.png' /><span>Rabbit API</span></Flex></Link>
                 <Spacer />
 
                 <Flex columnGap={5} alignItems='center'>
@@ -15,7 +15,7 @@ function Navbar() {
                     <button
                         style={{
                             fontSize: "20px",
-                            transform: colorMode==='light'?"rotate(0deg)":"rotate(30deg)"
+                            transform: colorMode === 'light' ? "rotate(0deg)" : "rotate(30deg)"
                         }}
                         title="Click to toggle the mode."
                         onClick={toggleColorMode}
