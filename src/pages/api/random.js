1ac8b5472792adf183ba9b9d95045c01fc1a2bac
sougata <sougataghar47@gmail.com>
2024-02-18 23:40:25 +0530

    changed variable name

--- src/pages/api/random.js
+++ src/pages/api/random.js
@@ -1,7 +1,7 @@
 import db from "../../utils/mongo";
 export default async function handler(req, res) {
     let random = require('lodash.random');
-    let books = await db.collection("rabbits").find().toArray();
-    res.status(200).json(books[random(0,books.length-1)])
+    let rabbits = await db.collection("rabbits").find().toArray();
+    res.status(200).json(rabbits[random(0,rabbits.length-1)])
   }
  
