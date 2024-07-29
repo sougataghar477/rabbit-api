ca204d4acee85a9298ddc2ecab57014342de0057
sougata <sougataghar47@gmail.com>
2024-07-30 00:50:10 +0530

    updated navbar color

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ import Link from "next/link";
 function Navbar() {
     const { colorMode, toggleColorMode } = useColorMode();
     return (
-        <Box bg={colorMode === "light" ? "#2CB0FF" : "blackAlpha.900"}>
+        <Box bg={colorMode === "light" ? "#2CB0FF" : "#172747"}>
             <Flex p={5} color={"white"} maxW={"960px"} mx={"auto"}>
                 <Link href="/"><Flex alignItems={'center'}><Image w={12} src='/rrrrabbit.png' /><span>Rabbit API</span></Flex></Link>
                 <Spacer />
@@ -15,7 +15,7 @@ function Navbar() {
                     <button
                         style={{
                             fontSize: "20px",
-                            transform: colorMode === 'light' ? "rotate(0deg)" : "rotate(30deg)"
+                          
                         }}
                         title="Click to toggle the mode."
                         onClick={toggleColorMode}
